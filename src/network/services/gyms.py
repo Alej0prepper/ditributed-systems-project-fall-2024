f1dcd1a2a1f55824e0bed73c74afd51b95fdbbca
Alejandro Alvarez <96664600+Alej0prepper@users.noreply.github.com>
2024-11-20 11:09:26 -0500

    gyms

---
+++ src/network/services/gyms.py
@@ -0,0 +1,74 @@
+from datetime import datetime 
+
+def create_gym_node(driver):
+    gym = driver.execute_query(
+        '''
+        CREATE (g:Gym)
+        RETURN id(g) AS gym_id
+     ''')
+
+def update_gym(driver, gym_id, name, email,location,address,styles, phone_number=None, ig_profile = None):
+
+    phone = phone_number if(phone_number) else ""
+    ig = ig_profile if(ig_profile) else ""
+
+    query = '''
+    MATCH (g:Gym)
+        WHERE id(g) = gym_id
+        SET g.name = $name,
+            g.email = $email,
+            g.location = $location,
+            g.address = $address,
+            g.styles = $styles,
+            g.phone_number = $phone,
+            g.ig_profile = $ig
+
+        RETURN g
+    '''
+
+    parameters = {
+        "name" : name,
+        "email" : email,
+        "location": location,
+        "address" : address,
+        "styles" : styles,
+        "phone" : phone,
+        "ig" : ig
+
+    }
+
+    result = driver.execute_query(query,parameters)
+    if(result):
+        print(f"Gym with ID {gym_id} updated successfully")
+    else:
+        print(f"Gym with ID {gym_id} not found or update failed")
+
+    def add_gym(driver,name, email,location,address,styles, phone_number=None, ig_profile = None):
+        gym_id = create_gym_node(driver)
+
+        update_gym(
+            driver,
+            gym_id,
+            name,
+            email,
+            location,
+            address,
+            styles,
+            phone_number=phone_number if phone_number else None,
+            ig_profile=ig_profile if ig_profile else None,
+        )
+        return gym_id
+
+    def get_gym_info(driver,gym_id):
+       
+        result = driver.execute_query(
+        """
+        MATCH (g:Gym) 
+            WHERE id(g) = $gym_id
+        RETURN g """,
+        {"gym_id": gym_id}
+        )
+
+
+
+        
