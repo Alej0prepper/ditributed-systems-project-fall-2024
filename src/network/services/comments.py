966b4f5be2cf1d45e2f2f19eabcdadc29bfd88f1
root <root@DESKTOP-HM7HE59>
2025-03-09 21:04:06 -0400

    add ids  to comments and reactions

--- src/network/services/comments.py
+++ src/network/services/comments.py
@@ -1,23 +1,25 @@
 from datetime import datetime
-
+import uuid
 # Comments
-def create_comment_node(driver, caption, media):
+def create_comment_node(driver, caption, media, user_id):
     now = datetime.now()
 
     query = """
-        CREATE (c:Comment {datetime: $now, caption: $caption, media: $media}) 
-        RETURN id(c) as comment_id
+        CREATE (c:Comment {datetime: $now, caption: $caption, media: $media,id:$id,userId: $userId }) 
+        RETURN c 
     """
     params = {
         "now": now,
         "caption": caption,
-        "media": media
+        "media": media,
+        "id": str(uuid.uuid4()),
+        "userId": user_id
     }
 
-    comment_id = driver.execute_query(query, params).records[0]["comment_id"]
+    comment_id = driver.execute_query(query, params).records[0]["id"]
     return comment_id
 
-def comment(driver, caption, media, username, answered_comment_id=None, commented_post_id=None):
+def comment(driver, caption, media, username,user_id, answered_comment_id=None, commented_post_id=None):
 
     if answered_comment_id:
         query = """
@@ -42,18 +44,18 @@ def comment(driver, caption, media, username, answered_comment_id=None, commente
         if len(driver.execute_query(query, params).records) == 0:
             return None, False, "Post not found."
 
-    new_comment_id = create_comment_node(driver, caption, media)
+    new_comment_id = create_comment_node(driver, caption, media,user_id)
 
     query = """
             MATCH (u:User {username: $username}) 
-            MATCH (c:Comment)
-                WHERE id(c) = $new_comment_id
-            CREATE (u) -[:Comments]-> (c)
+            MATCH (c:Comment {id:})
+            CREATE (u) -[:Comments{id:$comment_relation_id}]-> (c)
         """
 
     params = {
         "username": username,
-        "new_comment_id": new_comment_id
+        "new_comment_id": new_comment_id,
+        "comment_relation_id": str(uuid.uuid4())
     }
 
     driver.execute_query(query, params)
@@ -64,12 +66,13 @@ def comment(driver, caption, media, username, answered_comment_id=None, commente
                 WHERE id(a) = $new_comment_id 
             MATCH (c:Comment)
                 WHERE id(c) = $answered_comment_id
-            CREATE (c) -[:Has]-> (a)
+            CREATE (c) -[:Has{id:$has_id}]-> (a)
         """
 
         params = {
             "new_comment_id": new_comment_id,
-            "answered_comment_id": answered_comment_id
+            "answered_comment_id": answered_comment_id,
+            "has_id": str(uuid.uuid4())
         }
 
         driver.execute_query(query, params)
@@ -79,19 +82,20 @@ def comment(driver, caption, media, username, answered_comment_id=None, commente
         query = """
             MATCH (a: Comment {id:$new_comment_id})
             MATCH (p:Post {id:$commented_post_id})
-            CREATE (p) -[:Has]-> (a)
+            CREATE (p) -[:Has{id:$has_id}]-> (a)
         """
 
         params = {
             "new_comment_id": new_comment_id,
-            "commented_post_id": commented_post_id
+            "commented_post_id": commented_post_id,
+            "has_id": str(uuid.uuid4())
         }
 
         driver.execute_query(query, params)
         return new_comment_id, True, None
 
-def answer_comment(driver, caption, media, username, answered_comment_id):
-    return comment(driver, caption, media, username, answered_comment_id=answered_comment_id)
+def answer_comment(driver, caption, media, username,user_id, answered_comment_id):
+    return comment(driver, caption, media, username,user_id, answered_comment_id=answered_comment_id)
 
-def comment_post(driver, caption, media, username, commented_post_id):
-    return comment(driver, caption, media, username, commented_post_id=commented_post_id)
+def comment_post(driver, caption, media, username,user_id, commented_post_id):
+    return comment(driver, caption, media, username,user_id, commented_post_id=commented_post_id)
